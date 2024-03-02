7aa99073df39aa4979fd86383962000d3676b7c9
Rahil <rahilsardar234@gmail.com>
2024-03-02 23:16:43 +0530

    added nav bar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './NavBar.css'
+
+function NavBar() {
+  return (
+    <div>
+      <ul>
+        <li>HOME</li>
+        <li>ABOUT</li>
+        <li>PROTFOLIO</li>
+        <li>RESUME</li>
+        <li>CONTACT</li>
+      </ul>
+    </div>
+  )
+}
+
+export default NavBar
