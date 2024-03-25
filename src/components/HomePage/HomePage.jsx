cd7d590e1cee6fb0c0fbddbf2d8d7e2078ef71ba
Rahil <rahilsardar234@gmail.com>
2024-03-25 21:25:33 +0530

    commented image

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,38 +1,47 @@
 import React from "react";
 import "./HomePage.css";
 import Rahilsardarresume from "../../assets/cv/RahilSardar_RESUME.pdf"; // Import the CV file
-// import imag from "../../assets/img/IMG_2334.png"
+// import imag from "../../assets/img/IMG_2858.png";
 
 function Text() {
-  // Function to handle download CV
   const handleDownloadCV = () => {
-    // Initiating download by creating an anchor element
     const anchor = document.createElement("a");
-    anchor.href = Rahilsardarresume; // Set the href to the imported CV file
-    anchor.download = "RahilSardarRESUME.pdf"; // Set the download filename
-    anchor.click(); // Simulate a click on the anchor element
+    anchor.href = Rahilsardarresume;
+    anchor.download = "Rahil_Sardar_RESUME.pdf";
+    anchor.click();
   };
 
   return (
-    <>
-      <div id="text_container">
-        <text id="hellotext">-HELLO</text>
-        <text id="nametext">I'm <font color= "#ff013d">Rahil</font> Sardar</text>
-        <text id="abouttext">
-          This is Rahil Sardar Web Developer, UI/UX designer, and Embedded Engineer located in India, looking for working around the globe.
-        </text>
-        {/* Adding onClick event to trigger handleDownloadCV function */}
-        <button id="download_btn" onClick={handleDownloadCV}>DOWNLOAD CV</button>
-      </div>
-    </>
+    <div id="text_container">
+      <span id="hellotext">-HELLO</span>
+      <span id="nametext">
+        I'm <font color="#ff013d">Rahil</font> Sardar
+      </span>
+      <span id="abouttext">
+        This is Rahil Sardar Web Developer, UI/UX designer, and Embedded
+        Engineer located in India, looking for working around the globe.
+      </span>
+      <button id="download_btn" onClick={handleDownloadCV}>
+        DOWNLOAD CV
+      </button>
+    </div>
   );
 }
 
+// function Photo() {
+//   return (
+//     <div className="ph_container">
+//       <img id="ph1" src={imag} alt="person imag" />
+//     </div>
+//   );
+// }
+
 function HomePage() {
   return (
-    <>
+    <div className="main_conaine">
+      {/* <Photo /> */}
       <Text />
-    </>
+    </div>
   );
 }
 
