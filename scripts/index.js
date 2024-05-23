833483b04505e6a43a3984f80439de487edda6be
Rahil <rahilsardar234@gmail.com>
2024-05-24 01:07:24 +0530

    nav bar gradient is fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,3 @@
-// window.onload = function () {
-//     window.scrollTo(0, 0);
-//   }
-const navbar = document.querySelector('.navclass');
-// Listen for the scroll event
-window.addEventListener('scroll', function() {
-  if (window.scrollY > 10) { // adjust this value based on when you want the color to change
-    navbar.classList.add('scrolled');
-  } else {
-    navbar.classList.remove('scrolled');
-  }
-});
-
 // Listen for the click event on each link
 document.querySelectorAll('.navclass li a').forEach(function(link) {
   link.addEventListener('click', function(event) {
